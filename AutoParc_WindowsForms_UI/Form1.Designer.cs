f9769d759607d814d52ab9298bd0b70b75f117fc
Gheorghe <Gheorghe@GEUNIUS>
2025-04-08 19:39:51 +0300

    Citireea si afisarea din interfata

--- AutoParc_WindowsForms_UI/Form1.Designer.cs
+++ AutoParc_WindowsForms_UI/Form1.Designer.cs
@@ -34,10 +34,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
